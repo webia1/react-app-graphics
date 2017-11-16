fae0e3209b2f7105ad1764ed3459f384305632f1
Webia1 <eb@ebia.eu>
2017-11-16 15:57:20 +0100

    Prepared for Importing Graphic Library

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import EBIA from './assets/img/ebia.png';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={EBIA} alt="EBIA" />
+          <h1 className="App-title">
+            @Webia1 - Simple Graphic App Example - Timeseries
+          </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro" />
       </div>
     );
   }
